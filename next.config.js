05e8cf1da59d712c0a7351082e73257f67a0f9d9
sanzodown <sanzodown@hotmail.fr>
2025-01-22 13:49:13 +0100

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.myanimelist.net',
-                pathname: '/images/**',
-            },
-        ],
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xvmzbysdwvnooozisvoc.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
